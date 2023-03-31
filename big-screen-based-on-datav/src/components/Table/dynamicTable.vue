7cd559b8ec7793ac98223b85692eb39e68a951be
Martin Wang <65059824+MartinGsus99@users.noreply.github.com>
2023-03-31 16:18:13 +0800

    Update dynamicTable.vue
    设置表格背景透明

--- big-screen-based-on-datav/src/components/Table/dynamicTable.vue
+++ big-screen-based-on-datav/src/components/Table/dynamicTable.vue
@@ -1,63 +1,54 @@
 <template>
-<div class="mx-panel-table">
-    <el-table v-loading="isLoading" :data="listData" ref="multipleTable" stripe row-key="id" :row-class-name="tableRowClassName">
-        <el-table-column prop="name" label="名称"></el-table-column>
-        <el-table-column prop="number" label="数量"></el-table-column>
-    </el-table>
-</div>
+<el-table :data="tableData" style="width: 100%"  :show-header="false">
+    <el-table-column prop="date" label="日期">
+    </el-table-column>
+    <el-table-column prop="name" label="姓名">
+    </el-table-column>
+</el-table>
 </template>
 
+  
 <script>
 export default {
-    directives: {},
-
-    props: {
-
-        index: {
-            default: false,
-            type: Boolean
-        },
-        isLoading: {
-            default: false,
-            type: Boolean
-        },
-        listData: {
-            default: null,
-            type: Array
-        },
-
-    },
-    beforeUpdate() {
-
-    },
-    methods: {
-        tableRowClassName({
-            row,
-            rowIndex
-        }) {
-            if (rowIndex === 1) {
-                return 'warning-row';
-            } else if (rowIndex === 3) {
-                return 'success-row';
-            }
-            return '';
+    data() {
+        return {
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }]
         }
-    },
+    }
 }
 </script>
 
-<style lang="css">
-.mx-panel-table {
-    margin-bottom: 20px;
-    border-bottom: 0;
-    background-color: #131124;
+  
+<style>
+.el-table {
+    background-color: transparent;
 }
 
-.el-table .warning-row {
-    background: oldlace;
+.el-table td, .el-table th {
+  color: #bab9b9; /* 将字体颜色设置为黑色 */
 }
 
-.el-table .success-row {
-    background: #57a12f;
+.el-table__body th {
+  background-color: transparent;
+}
+
+.el-table__body tr {
+  background-color: transparent;
 }
 </style>
