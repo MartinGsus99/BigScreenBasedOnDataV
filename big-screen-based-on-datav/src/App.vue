3528a23e8c472189293774d02f1aedb904ec2bbe
Martin Wang <65059824+MartinGsus99@users.noreply.github.com>
2023-04-04 12:09:14 +0800

    添加Echarts图表自适应

--- big-screen-based-on-datav/src/App.vue
+++ big-screen-based-on-datav/src/App.vue
@@ -1,242 +1,250 @@
 <template>
-<div class="dashboard">
+  <div class="dashboard">
     <div class="left-container">
-        <div id="web-application-chart" class="widget-container-left">
-
-        </div>
-        <div id="web-frame-chart" class="widget-container-left">
-
-        </div>
-        <div id="web-polar-chart" class="widget-container-left-polar">
-
-        </div>
+      <div id="web-application-chart" class="widget-container-left"></div>
+      <div id="web-frame-chart" class="widget-container-left"></div>
+      <div id="web-polar-chart" class="widget-container-left-polar"></div>
     </div>
 
     <div class="middle-container">
-        <div class="header-container">
-            <staticBar :StaticData='StaticData' :timeShow="false"></staticBar>
-        </div>
-        <div class="map-container">
-            <MapCharts></MapCharts>
-        </div>
+      <div class="header-container">
+        <staticBar :StaticData="StaticData" :timeShow="false"></staticBar>
+      </div>
+      <div class="map-container">
+        <MapCharts></MapCharts>
+      </div>
     </div>
 
     <div class="right-container">
-        <div id="web-qeupiment-chart" class="widget-container-right">
-
-        </div>
-        <div id="web-port-chart" class="widget-container-right">
-
-        </div>
-        <div class="widget-container-right">
-            <div style="color:white">组件</div>
-            <Table :index="true" :listData="listData" :uiData="uiData"></Table>
-        </div>
-        <div id="web-point-chart" class="widget-container-right">
-
-        </div>
+      <div id="web-qeupiment-chart" class="widget-container-right"></div>
+      <div id="web-port-chart" class="widget-container-right"></div>
+      <div class="widget-container-right">
+        <div style="color: white">组件</div>
+        <Table :index="true" :listData="listData" :uiData="uiData"></Table>
+      </div>
+      <div id="web-point-chart" class="widget-container-right"></div>
     </div>
-</div>
+  </div>
 </template>
 
 <script>
-import MapCharts from './components/DataContainer/MapComponent.vue';
-import Table from './components/Table/dynamicTable.vue';
-import StaticBar from './components/StaticData/index.vue';
+import MapCharts from "./components/DataContainer/MapComponent.vue";
+import Table from "./components/Table/dynamicTable.vue";
+import StaticBar from "./components/StaticData/index.vue";
 import {
-    webOption,
-    webFrameOption,
-    webContainerOption,
-    webEquipmentOption,
-    portOption,
-    pointOption
-} from './config/EchartsConfig';
+  webOption,
+  webFrameOption,
+  webContainerOption,
+  webEquipmentOption,
+  portOption,
+  pointOption,
+} from "./config/EchartsConfig";
 
 export default {
-    components: {
-        MapCharts,
-        Table,
-        StaticBar
-    },
-    data() {
-        return {
-            StaticData: [{
-                    key: "site",
-                    value: 3287303017,
-                    title: "站点",
-                    precision: 0,
-                    tag: "个",
-                    preIcon: "el-icon-top",
-                    style: "color:red",
-                    type: "value",
-                },
-                {
-                    key: "qeuipment",
-                    value: 5927696468,
-                    title: "设备",
-                    preIcon: "el-icon-top",
-                    style: "color:red",
-                    type: "value",
-                },
-                {
-                    key: "component",
-                    value: 146832,
-                    title: "组件",
-                    type: "value",
-                },
-            ],
-            listData: [{
-                    name: 'Unknown',
-                    number: 123456
-                },
-                {
-                    name: 'Apache',
-                    number: 123456
-                },
-                {
-                    name: 'Nginx',
-                    number: 123456
-                },
-
-            ],
-            uiData: [{
-                    label: '名称',
-                    prop: 'name',
-                },
-                {
-                    label: '数量',
-                    prop: 'number'
-                }
-            ],
-            charts:[],
-        }
-    },
-    mounted() {
-        window.addEventListener('resize', this.handleResize)
-        this.generateWebChart();
-        this.generateWebFrameChart();
-        this.generateWebFramePolarChart();
-        this.generateWebqEupimentPolarChart();
-        this.generateWebqPortPolarChart();
-        this.generateWebqPointPolarChart();
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.handleResize)
-    },
-    destroyed: function () {
-        /*
+  components: {
+    MapCharts,
+    Table,
+    StaticBar,
+  },
+  data() {
+    return {
+      windowWidth: 0,
+      windowHeight: 0,
+      StaticData: [
+        {
+          key: "site",
+          value: 3287303017,
+          title: "站点",
+          precision: 0,
+          tag: "个",
+          preIcon: "el-icon-top",
+          style: "color:red",
+          type: "value",
+        },
+        {
+          key: "qeuipment",
+          value: 5927696468,
+          title: "设备",
+          preIcon: "el-icon-top",
+          style: "color:red",
+          type: "value",
+        },
+        {
+          key: "component",
+          value: 146832,
+          title: "组件",
+          type: "value",
+        },
+      ],
+      listData: [
+        {
+          name: "Unknown",
+          number: 123456,
+        },
+        {
+          name: "Apache",
+          number: 123456,
+        },
+        {
+          name: "Nginx",
+          number: 123456,
+        },
+      ],
+      uiData: [
+        {
+          label: "名称",
+          prop: "name",
+        },
+        {
+          label: "数量",
+          prop: "number",
+        },
+      ],
+      charts: [],
+    };
+  },
+  mounted() {
+    
+    window.addEventListener("resize", this.handleResize);
+     // 初始化获取窗口大小
+     this.getWindowSize()
+    this.generateWebChart();
+    this.generateWebFrameChart();
+    this.generateWebFramePolarChart();
+    this.generateWebqEupimentPolarChart();
+    this.generateWebqPortPolarChart();
+    this.generateWebqPointPolarChart();
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  destroyed: function () {
+    /*
         在调用destroyed的时候, 表示当前组件已经被销毁了
         注意点: 只要组件不被销毁, 那么destroyed就不会调用
                 不要在这个生命周期方法中再去操作组件中数据和方法
         * */
-        clearInterval(this.refresh);
-        this.refresh = null;
-
+    clearInterval(this.refresh);
+    this.refresh = null;
+  },
+  methods: {
+    handleResize() {
+      for (let i = 0; i < this.charts.length; i++) {
+        this.charts[i].resize();
+      }
     },
-    methods: {
-        handleResize() {
-            for(let i=0;i<this.charts.length;i++){
-                this.charts[i].resize();
-            }
-        },
-        refreshChart(chartName, option) {
-            var chart = document.getElementById(chartName);
-            var chartInstance = this.$echarts.getInstanceByDom(chart);
-            chartInstance.setOption(option);
-        },
-
-        generateWebChart() {
-            var myChart = this.$echarts.init(document.getElementById('web-application-chart'));
-            myChart.setOption(webOption);
-            this.charts.push(myChart);
-        },
-
-        generateWebFrameChart() {
-            var myChart = this.$echarts.init(document.getElementById('web-frame-chart'));
-            myChart.setOption(webFrameOption);
-            this.charts.push(myChart);
-        },
-
-        generateWebFramePolarChart() {
-            var myChart = this.$echarts.init(document.getElementById('web-polar-chart'));
-            myChart.setOption(webContainerOption);
-            this.charts.push(myChart);
-        },
-
-        generateWebqEupimentPolarChart() {
-            var myChart = this.$echarts.init(document.getElementById('web-qeupiment-chart'));
-            myChart.setOption(webEquipmentOption);
-            this.charts.push(myChart);
-        },
-
-        generateWebqPortPolarChart() {
-            var myChart = this.$echarts.init(document.getElementById('web-port-chart'));
-            myChart.setOption(portOption);
-            this.charts.push(myChart);
-        },
-
-        generateWebqPointPolarChart() {
-            var myChart = this.$echarts.init(document.getElementById('web-point-chart'));
-            myChart.setOption(pointOption);
-            this.charts.push(myChart);
-        },
-
+    getWindowSize() {
+      // 获取窗口大小
+      this.windowWidth = window.innerWidth
+      this.windowHeight = window.innerHeight
     },
-}
+    refreshChart(chartName, option) {
+      var chart = document.getElementById(chartName);
+      var chartInstance = this.$echarts.getInstanceByDom(chart);
+      chartInstance.setOption(option);
+    },
+
+    generateWebChart() {
+      var myChart = this.$echarts.init(
+        document.getElementById("web-application-chart")
+      );
+      myChart.setOption(webOption);
+      this.charts.push(myChart);
+    },
+
+    generateWebFrameChart() {
+      var myChart = this.$echarts.init(
+        document.getElementById("web-frame-chart")
+      );
+      myChart.setOption(webFrameOption);
+      this.charts.push(myChart);
+    },
+
+    generateWebFramePolarChart() {
+      var myChart = this.$echarts.init(
+        document.getElementById("web-polar-chart")
+      );
+      myChart.setOption(webContainerOption);
+      this.charts.push(myChart);
+    },
+
+    generateWebqEupimentPolarChart() {
+      var myChart = this.$echarts.init(
+        document.getElementById("web-qeupiment-chart")
+      );
+      myChart.setOption(webEquipmentOption);
+      this.charts.push(myChart);
+    },
+
+    generateWebqPortPolarChart() {
+      var myChart = this.$echarts.init(
+        document.getElementById("web-port-chart")
+      );
+      myChart.setOption(portOption);
+      this.charts.push(myChart);
+    },
+
+    generateWebqPointPolarChart() {
+      var myChart = this.$echarts.init(
+        document.getElementById("web-point-chart")
+      );
+      myChart.setOption(pointOption);
+      this.charts.push(myChart);
+    },
+  },
+};
 </script>
 
 <style>
 body {
-    margin: 0;
-    padding: 0;
-    background: #131124;
-    overflow: hidden;
+  margin: 0;
+  padding: 0;
+  background: #131124;
+  overflow: hidden;
 }
 
 .dashboard {
-    display: flex;
-    flex-direction: row;
-    padding: 10px;
-    background-color: #131124;
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  background-color: #131124;
 }
 
 .middle-container {
-    width: 50%;
-    height: 100%;
-
+  width: 50%;
+  height: 100%;
 }
 
 .header-container {
-    height: 120px;
-    margin: 30px;
+  height: 120px;
+  margin: 30px;
 }
 
 .map-container {
-    height: 100%;
+  height: 100%;
 }
 
 .left-container {
-    display: flex;
-    flex-direction: column;
-    width: 25%;
+  display: flex;
+  flex-direction: column;
+  width: 25%;
 }
 
 .right-container {
-    display: flex;
-    flex-direction: column;
-    width: 25%;
+  display: flex;
+  flex-direction: column;
+  width: 25%;
 }
 
 .widget-container-left {
-    flex-grow: 1;
+  flex-grow: 1;
 }
 
 .widget-container-left-polar {
-    flex-grow: 2;
+  flex-grow: 2;
 }
 
 .widget-container-right {
-    flex-grow: 1;
+  flex-grow: 1;
 }
-
 </style>
--- big-screen-based-on-datav/src/components/Table/dynamicTable.vue
+++ big-screen-based-on-datav/src/components/Table/dynamicTable.vue
@@ -1,47 +1,56 @@
 <template>
-<el-table :data="tableData" style="width: 100%"  :show-header="false">
-    <el-table-column prop="date" label="日期">
-    </el-table-column>
-    <el-table-column prop="name" label="姓名">
-    </el-table-column>
-</el-table>
+  <el-table :data="tableData" style="width: 100%" :show-header="false">
+    <el-table-column prop="name"> </el-table-column>
+    <el-table-column prop="nums" :formatter="formatNums"> </el-table-column>
+  </el-table>
 </template>
 
-  
 <script>
 export default {
-    data() {
-        return {
-            tableData: [{
-                date: '2016-05-02',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1518 弄'
-            }, {
-                date: '2016-05-04',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1517 弄'
-            }, {
-                date: '2016-05-01',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1519 弄'
-            }, {
-                date: '2016-05-03',
-                name: '王小虎',
-                address: '上海市普陀区金沙江路 1516 弄'
-            }]
-        }
-    }
-}
+  data() {
+    return {
+      tableData: [
+        {
+          name: "Unknown",
+          nums: 123456789,
+        },
+        {
+          name: "Apache",
+          nums: 123456789,
+        },
+        {
+          name: "Ajax",
+          nums: 123456789,
+        },
+        {
+          name: "Spring",
+          nums: 123456789,
+        },
+      ],
+    };
+  },
+  methods: {
+    formatNums(row, column, cellValue) {
+        console.log(cellValue);
+        if (typeof cellValue === 'number') {
+                    return cellValue.toLocaleString(); // 使用千位分隔符的格式显示数字
+                } else {
+                    return cellValue;
+                }
+    },
+  },
+};
 </script>
 
-  
 <style>
 .el-table {
-    background-color: transparent;
+  background-color: transparent;
 }
 
-.el-table td, .el-table th {
-  color: #bab9b9; /* 将字体颜色设置为黑色 */
+.el-table td,
+.el-table th {
+  color: #bab9b9;
+  /* 将字体颜色设置为黑色 */
 }
 
 .el-table__body th {
@@ -50,5 +59,9 @@ export default {
 
 .el-table__body tr {
   background-color: transparent;
+}
+
+.el-table__row:hover {
+  background-color: transparent ;
 }
 </style>
